aca7cbee4e9c10ab3dd93ee0d360e21ef496cec2
ganesh <tarigondaganesh1234@gmail.com>
2025-12-23 20:57:34 +0530

    p

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 from PIL import Image
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from io import BytesIO
